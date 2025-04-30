4c5534991e2cb244c468b7f8793d1eb98438982b
BinomialSheep <jose_jinrou@yahoo.co.jp>
2025-04-30 20:39:03 +0900

    baseパスの修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // GitHub Pages の URL
+  base: '/visualizer_masters2025_final/',   // ←★ここを必ず設定
   plugins: [react(), tsconfigPaths()],
 });
